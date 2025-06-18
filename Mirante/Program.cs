7349b1d93d57034d0aef4b9d5eae92901b57c03e
Mateus Chagas de Oliveira <mateuschagasoliveira@gmail.com>
2025-06-18 17:54:02 -0300

    Feture: configuration of the connection Sercer

--- Mirante/Program.cs
+++ Mirante/Program.cs
@@ -1,17 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using Mirante.Application.UnitOfWork;
 using Mirante.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-
 builder.Services.AddDbContext<ToDoContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
